fe73dbbd9dcb9d0d38acfd7b065a183ba6efb52d
KyryloKalinichenko <Kirillstomper@gmail.com>
2021-11-16 13:47:57 +0100

    images

--- blog/blog/mainpage/models.py
+++ blog/blog/mainpage/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    img_name = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    photo = models.ImageField(upload_to='media', default=0)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
 class Programme(models.Model):
     title = models.CharField(max_length=255)
-    img_name = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    photo = models.ImageField(upload_to='media', default=0)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
--- blog/blog/mainpage/urls.py
+++ blog/blog/mainpage/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 # from . import views
 from .views import Blogline, DetailPost, DetailProgramme, Home, Sources, About
+
 
 urlpatterns = [
     # path('', views.home, name='home'),
@@ -9,4 +12,10 @@ urlpatterns = [
     path('sources', Sources.as_view(), name='sources'),
     path('about', About, name='about'),
     path('article/<int:pk>', DetailPost.as_view(), name='detail')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     # static files (images, css, javascript, etc.)
+#     urlpatterns += patterns('',
+#         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+#         'document_root': settings.MEDIA_ROOT}))
