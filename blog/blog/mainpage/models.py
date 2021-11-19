db5f273a7f950b5a6dc0ab3c8a028cbcd341f309
Kyrylo Kalinichenko <kirillstomper@gmail.com>
2021-11-19 12:06:25 +0100

    ok

--- blog/blog/mainpage/models.py
+++ blog/blog/mainpage/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
-    photo = models.ImageField(upload_to='media', default=0)
+    photo = models.ImageField(upload_to='media/materials', default=0)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
@@ -14,7 +14,7 @@ class Programme(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
-    photo = models.ImageField(upload_to='media', default=0)
+    photo = models.ImageField(upload_to='media/posts', default=0)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
